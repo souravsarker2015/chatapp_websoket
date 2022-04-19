60996a1aa2f0f34da8a9be09469db5aa3209400f
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-19 14:56:01 +0600

    chat app consumer.py routing.py

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
+import threading
 
 
 def index(request):
+    print(threading.get_native_id())
     return render(request, 'chat/index.html')
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
