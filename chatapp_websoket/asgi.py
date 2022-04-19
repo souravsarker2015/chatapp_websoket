60ce2b5cf6a46beaa3b5e1f3c1a8c336afc5d8fe
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-19 14:23:48 +0600

    chat app using websoket

--- chatapp_websoket/asgi.py
+++ chatapp_websoket/asgi.py
@@ -1,16 +1,28 @@
-"""
-ASGI config for chatapp_websoket project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
-
+import django
+from channels.http import AsgiHandler
 from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp_websoket.settings')
+django.setup()
+from channels.auth import AuthMiddlewareStack
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+})
+
+#
+#
+# import os
+#
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp_websoket.settings')
+#
+# application = get_asgi_application()
